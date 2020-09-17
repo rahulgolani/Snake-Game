bf53e7f84b5694993209008e7221a0983096e3fc
Rahul Golani <rahulgolani24@live.com>
2020-09-17 21:08:01 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import pygame
+
+# initialize the imported pygame modules, returns the tuple of successful and failed imports
+pygame.init()
+
+# initializing the gamescreen
+gameScreen=pygame.display.set_mode(size=(640,480))
